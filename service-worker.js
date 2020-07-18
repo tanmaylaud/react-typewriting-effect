b3ab535002bab9fd9a411f5030b619c3a6ec1189
Tanmay <tanmaylaud@gmail.com>
2020-07-18 17:20:10 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-typewriting-effect/precache-manifest.31c1a2d088b7eccd53c722bcaa6ec9bb.js"
+  "/react-typewriting-effect/precache-manifest.48570e17f67636c6e3f938141b5183ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-typewriting-effect/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
